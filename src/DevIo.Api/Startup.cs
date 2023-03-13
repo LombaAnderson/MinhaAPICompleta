17317ab3da68b9bdc40c232dbe1404c4a60319f2
Anderson Lomba <andersonlomba172@outlook.com>
2023-03-13 14:24:03 -0300

    Adicionar arquivos de projeto.

---
+++ src/DevIo.Api/Startup.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace DevIo.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<ApiDbContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+        }
+    }
+}
+
