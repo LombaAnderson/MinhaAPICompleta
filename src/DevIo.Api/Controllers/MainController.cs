678d5ac512b61c99caae30db315f4ba4a2088ca5
Anderson Lomba <andersonlomba172@outlook.com>
2023-03-14 19:53:40 -0300

    Primeiro commit

---
+++ src/DevIo.Api/Controllers/MainController.cs
@@ -0,0 +1,33 @@
+﻿using DevIo.Api.ViewModels;
+using DevIO.Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIo.Api.Controllers
+{
+
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+
+        // Validação de notificações de erro
+
+        // Validação de modelstate 
+
+        // Validação de operação de negócios
+
+    }
+
+    [Route("api/fornecedores")]
+    public  class FornecedoresController : MainController
+    {
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+            var fornecedor = await FornecedorRepository.ObterTodos();
+            
+            
+            
+            return Ok();
+        }
+    }
+
+}
